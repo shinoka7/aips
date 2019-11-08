3e5b5dd0f94bb34009acd83c9d8da2c2fa7aee6b
shinoka7 <okas@rpi.edu>
2019-11-08 15:49:01 -0500

    group details page

--- pages/group/show.jsx
+++ pages/group/show.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button } from 'reactstrap';
+import { Button, Badge, Jumbotron, Spinner } from 'reactstrap';
 
 class GroupDetail extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            isVerified: false,
+        };
+
+        // this.toggleVerifyPanel = this.toggleVerifyPanel.bind(this);
     }
 
     static async getInitialProps(context) {
         return context.query || {};
     }
 
+    toggleVerifyPanel() {
+        
+    }
+
     render() {
+        const { group } = this.props;
+        const { isVerified } = this.state;
+
         return (
             <div>
-                Group Page
+                <Jumbotron>
+                    <h2 className="display-3">{group.name}</h2>
+                    { isVerified &&
+                        <Badge /**onClick={this.toggleVerifyPanel}*/ color="success" pill>Verified</Badge>
+                    }
+                    { !isVerified &&
+                            <Badge /**onClick={this.toggleVerifyPanel}*/ color="warning" pill>Pending</Badge>
+                    }
+                    <Spinner style={{ width: '3rem', height: '3rem' }} />{' '}
+                </Jumbotron>
             </div>
         )
     }
