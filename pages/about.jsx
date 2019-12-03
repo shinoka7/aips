5705d02986e08389430011b33aa64ab2472a5415
Jeremy Wisbey <wisbej@rpi.edu>
2019-12-03 11:11:11 -0500

    Updated the about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -12,8 +12,22 @@ class About extends React.Component {
 
     render() {
         return (
-            <div className="pt-3">
-                <p>AIPS is meant to create clubs and manage activities, while keeping with other members</p>
+            <div className="about col-sm-12 col-md-6 offset-md-3">
+                <h2>An Intuitive Platform for Societies, or AIPS for short</h2>
+                <br/>
+                <p>
+                    AIPS is a central location for posting, advertising, and tracking club events and updates.
+                    We designed the app to solve the problem of how complicated it is for RPI societies to advertise public events.
+                    With so many social media apps, it's hard for an interested student to keep on top of every different RPI event throughout the week.
+                    AIPS simplifies the event-making functionality of Facebook, personalized for RPI students.
+                </p>
+                <br/>
+                <p>
+                    Once you add your society to AIPS, you can create public events on the calendar.
+                    Click on the Groups tab to get started.
+                </p>
+                <br/>
+                <p>Check out our <a href="https://github.com/shinoka7/aips">Github</a> to learn about contributing, create issues, or contact us.</p>
             </div>
         );
     }
