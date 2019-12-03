97e64cbedc0895e9fcd5395db2d0b7bda603731b
Jeremy Wisbey <wisbej@rpi.edu>
2019-12-03 14:45:18 -0500

    Updated login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { Input, Button } from 'reactstrap';
 
 export default class extends React.Component {
-    
+
     static async getInitialProps(context) {
         return context.query || {};
     }
 
     render() {
         return (
-            <div>
-                <span>Login Page</span>
+            <div className="about col-sm-12 col-md-4 offset-md-4">
+                <h4>Login</h4>
                 <hr/>
                 <div>
                     <form action="/auth/google" method="GET">
                         <Button className="btn btn-primary"><i className="fab fa-google"></i> Login with Google</Button>
                     </form>
+                    <br/>
                     <form action="/auth/cas" method="GET">
                         {/* <input type="hidden" name="redirectTo" value="/auth/cas/callback" /> */}
                         <Button className="btn btn-primary"><i className="rpi-icon"> Login with CAS</i></Button>
