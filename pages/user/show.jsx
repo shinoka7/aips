75a622ec3ac7fc9cad0f4993699bb3194015a003
shinoka7 <okas@rpi.edu>
2020-02-04 14:41:43 -0500

    added user page individual calendars

--- pages/user/show.jsx
+++ pages/user/show.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Jumbotron } from 'reactstrap';
+import { Jumbotron, Button, ButtonGroup } from 'reactstrap';
 
 import UserProfile from '../../src/client/components/user/UserProfile';
 import GroupList from '../../src/client/components/group/GroupList';
+import CalendarPanel from '../../src/client/components/CalendarPanel.jsx';
 
 class UserDetail extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            calendarIsOpen: false,
         }
+
+        this.toggleCalendar = this.toggleCalendar.bind(this);
     }
     
     static async getInitialProps(context) {
         return context.query || {};
     }
 
+    toggleCalendar() {
+        this.setState({ calendarIsOpen: !this.state.calendarIsOpen });
+    }
+
     render() {
-        const { user, notifications, csrfToken } = this.props;
+        const { user, notifications, csrfToken, events, groups, images } = this.props;
+        const { calendarIsOpen } = this.state;
 
         return (
             <div>
@@ -31,7 +40,14 @@ class UserDetail extends React.Component {
                         {/* TODO make cool arrow button && transition Dropdown? */}
                     {/* </Button> */}
                     <hr />
+                    <ButtonGroup className="text-right">
+                        <Button onClick={this.toggleCalendar} className="btn btn-info">
+                            <i className="fas fa-calendar-alt"> Events</i>
+                        </Button>
+                    </ButtonGroup>
+                    <CalendarPanel toggleCalendar={this.toggleCalendar} events={events} groups={groups} csrfToken={csrfToken} modal={calendarIsOpen} images={images} />
                     <GroupList notifications={notifications} csrfToken={csrfToken} />
+                    {console.log(events)}
                 </Jumbotron>
             </div>
         );
@@ -42,6 +58,9 @@ UserDetail.propTypes = {
     user: PropTypes.object.isRequired,
     notifications: PropTypes.arrayOf(PropTypes.object).isRequired,
     csrfToken: PropTypes.string.isRequired,
+    events: PropTypes.arrayOf(PropTypes.object).isRequired,
+    groups: PropTypes.arrayOf(PropTypes.object).isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default UserDetail;
