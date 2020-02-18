01aca27605476ef21bd1af520f3a20077e5a81f2
shinoka7 <42939055+shinoka7@users.noreply.github.com>
2020-02-18 17:19:41 -0500

    start of backend implementation for google calendar sync

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { google } = require('googleapis');
+
+const { body } = require('express-validator');
+const validator = {};
 
 const googleService = require('../../services/google');
 const cas = require('../../services/cas');
@@ -12,6 +16,7 @@ module.exports = (aips) => {
   
   const {
     async: asyncMiddleware,
+    validateBody,
   } = middlewares;
 
   router.get('/google', (req, res) => {
@@ -29,15 +34,76 @@ module.exports = (aips) => {
       return res.status(404).send({ error: 'user not found' });
     }
     const { code } = req.query;
-    const { id, email, tokens, calendar }  = await googleService.getGoogleAccountFromCode(code);
+    // const { id, email, tokens, calendar }  = await googleService.getGoogleAccountFromCode(code);
+    const calendar = await googleService.getGoogleAccountFromCode(code);
     // const account = await Account.createOrLink('google', id, email);
     // const account = await Account.linkAccount('google', id, user.id);
     // req.session.user = account.User;
     // console.log(calendar.calendarList.list());
+    console.log(calendar.calendarList.list());
     req.session.calendar = calendar;
     req.session.save((err) => {
       res.redirect('/user');
     });
+  }));
+
+  validator.addToCalendar = [
+    body('groupId').exists(),
+    body('startDate').not().isEmpty().trim(),
+    body('startTime').not().isEmpty().trim(),
+    body('endDate').not().isEmpty().trim(),
+    body('endTime').not().isEmpty().trim(),
+    body('name').not().isEmpty().trim(),
+    body('description').exists().trim(),
+    body('Group').exists(),
+  ];
+
+  router.post('/google/addToCalendar', csrf, validator.addToCalendar, validateBody, asyncMiddleware(async(req, res) => {
+    // Create an authorized client
+    try {
+      const auth = googleService.createConnection();
+      const googleCalendar = google.calendar({ version: 'v3', auth });
+      
+      console.log(googleCalendar.calendarList.list());
+
+      const { groupId, startDate, startTime, endDate, endTime, name, description, image, Group } = req.body;
+      const userId = req.session.user.id;
+      const user = await User.findByPk(userId);
+      if (!user) {
+          return res.status(404).send({ error: 'User not found' });
+      }
+
+      const newEvent = {
+        summary: name,
+        description: description,
+        organizer: {
+            displayName: Group.name,
+            email: Group.groupEmail,
+        },
+        start: {
+            date: startDate
+        },
+        end: {
+            date: endDate
+        },
+      };
+
+      await googleCalendar.events.insert({
+          calendarId: 'primary',
+          resource: newEvent,
+        }, function(err, newEvent) {
+          if (err) {
+            console.log('There was an error contacting the Calendar service: ' + err);
+            return;
+          }
+          console.log('Event created: %s', newEvent.htmlLink);
+        });
+
+      res.json({ googleCalendar });
+    }
+    catch(err) {
+      console.log(err);
+    }
   }));
 
   router.get('/cas', cas.bounce, asyncMiddleware(async(req, res) => {
--- src/services/google.js
+++ src/services/google.js
@@ -54,12 +54,13 @@ class GoogleService {
         const me = await plus.people.get({ userId: 'me' });
         const userGoogleId = me.data.id;
         const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value;
-        return {
-          id: userGoogleId,
-          email: userGoogleEmail,
-          tokens: tokens,
-          calendar: calendar,
-        };
+        return calendar;
+        // return {
+        //   id: userGoogleId,
+        //   email: userGoogleEmail,
+        //   tokens: tokens,
+        //   calendar: calendar,
+        // };
     }
 
 }
